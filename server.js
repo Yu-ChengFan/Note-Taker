076fa95db1f1313a16b09eb4ff3c92871e73b5c5
Corny Mac <kevin130366@gmail.com>
2021-12-05 18:27:59 -0700

    add heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const apiRoute = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`Express server started on port ${PORT}`);
+});
